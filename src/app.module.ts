b5bcfb4ef770ad7541cd82c4429831240c9fcbf3
Yoni <firroloni.yoni@gmail.com>
2021-05-04 00:35:06 +0200

    fix gitignore

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@picspy-mongo:${process.env.MONGO_PORT}`,
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
