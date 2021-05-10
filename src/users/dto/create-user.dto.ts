898758f52ee8f8c2df724c159168a46c107c79f8
Yoni <firroloni.yoni@gmail.com>
2021-05-10 16:44:21 +0200

    reversing...

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -2,11 +2,11 @@ import { IsNotEmpty, Matches } from 'class-validator';
 
 export class CreateUserDTO {
   @IsNotEmpty()
-  username: String;
+  username: string;
 
   @IsNotEmpty()
   @Matches(
     /^(?=.{8,})(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[#?!@$ %^&*-])([a-zA-Z#?!@$ %^&*-]*\d){3,}/,
   )
-  password: String;
+  password: string;
 }
