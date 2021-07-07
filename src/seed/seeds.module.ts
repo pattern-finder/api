230624c55a9836e9056faa698e6cb0f9133afff0
Yoni <firroloni.yoni@gmail.com>
2021-07-07 10:46:55 +0200

    add route for seed

--- src/seed/seeds.module.ts
+++ src/seed/seeds.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommandModule } from 'nestjs-command';
 import { dbConfig } from 'src/db/db.config';
@@ -10,7 +10,7 @@ import { LanguagesCommand } from './languages.command';
   imports: [
     MongooseModule.forRoot(dbConfig.url),
     CommandModule,
-    LanguagesModule,
+    forwardRef(() => LanguagesModule),
   ],
   providers: [LanguagesCommand],
   exports: [LanguagesCommand],
