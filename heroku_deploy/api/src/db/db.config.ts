37e587fd57216c97d82993a8e64af27e4e751cdf
tcatonet <catonethomas@yahoo.fr>
2021-09-19 17:48:20 +0200

    update heroku.yml

--- heroku_deploy/api/src/db/db.config.ts
+++ heroku_deploy/api/src/db/db.config.ts
@@ -1,3 +1,3 @@
 export const dbConfig = {
-  url: `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}`,
+  url: `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:${process.env.PORT}`,
 };
