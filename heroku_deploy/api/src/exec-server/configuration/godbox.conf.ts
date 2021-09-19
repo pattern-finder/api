345e1e3aab0563a45cdba0e849077ae4778deedb
tcatonet <catonethomas@yahoo.fr>
2021-09-20 00:32:51 +0200

    update dockerfile godbox

--- heroku_deploy/api/src/exec-server/configuration/godbox.conf.ts
+++ heroku_deploy/api/src/exec-server/configuration/godbox.conf.ts
@@ -1,3 +1,3 @@
 export const godboxConfig = {
-  baseUrl: `http://${process.env.GODBOX_URL || 'picspy-godbox'}:${process.env.PORT}`,
+  baseUrl: `http://${process.env.GODBOX_URL || 'picspy-godbox'}:${process.env.PORT||3000}`,
 };
