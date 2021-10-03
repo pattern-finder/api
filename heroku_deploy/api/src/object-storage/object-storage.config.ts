46b722eceaa2bca05ee83de0784de0ec7514eff6
tcatonet <catonethomas@yahoo.fr>
2021-10-04 01:25:55 +0200

    test adresse minio

--- heroku_deploy/api/src/object-storage/object-storage.config.ts
+++ heroku_deploy/api/src/object-storage/object-storage.config.ts
@@ -2,8 +2,8 @@ export const config = {
   MINIO_INTERNAL_ENDPOINT:
     process.env.MINIO_INTERNAL_ENDPOINT || 'picspy-minio',
   MINIO_EXTERNAL_ENDPOINT: process.env.MINIO_EXTERNAL_ENDPOINT || 'localhost',
-  MINIO_INTERNAL_PORT: Number(process.env.MINIO_INTERNAL_PORT) || 9000,
-  MINIO_EXTERNAL_PORT: Number(process.env.MINIO_EXTERNAL_PORT) || 9001,
+  MINIO_INTERNAL_PORT: Number(process.env.MINIO_INTERNAL_PORT),// || 9000,
+  MINIO_EXTERNAL_PORT: Number(process.env.MINIO_EXTERNAL_PORT),// || 9001,
   MINIO_ACCESSKEY: process.env.MINIO_ACCESS_KEY || 'miniokey',
   MINIO_SECRETKEY: process.env.MINIO_SECRET_KEY || 'miniosecret',
   MINIO_REGION: process.env.MINIO_REGION || '',
