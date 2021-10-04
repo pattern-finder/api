2c545e3f4f830a511ce1b9ec1ed879ed9f68422d
tcatonet <catonethomas@yahoo.fr>
2021-10-04 14:41:52 +0200

    transition heroku google cloud

---
+++ src/db/db.config.ts
@@ -0,0 +1,4 @@
+export const dbConfig = {
+ // url: `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.HOST}/${process.env.MONGO_DB}${process.env.OPTION}`,
+  url: `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}`,
+};
