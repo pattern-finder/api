cec56ec859768b28f41602b69f7e1e4986aa0abc
tcatonet <catonethomas@yahoo.fr>
2021-10-09 23:46:34 +0200

    logs node

--- heroku_deploy/api/src/pictures/pictures.service.ts
+++ heroku_deploy/api/src/pictures/pictures.service.ts
@@ -41,10 +41,10 @@ export class PicturesService {
     challenge: string,
     fromInternal = false,
   ): Promise<PictureUrlDTO[]> {
-    console.log("COUCOU1")
 
     const pictures = (await this.pictureModel.find({ challenge }).exec()).map(
       (picture) => {
+        console.log("STOP")
 
         const pictureObject = picture.toObject();
         const file = fromInternal
@@ -56,7 +56,7 @@ export class PicturesService {
               pictureObject.url,
             );
 
-        console.log(pictureObject.url)
+        console.log(pictureObject)
 
         return {
           _id: pictureObject._id,
