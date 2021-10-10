b214bb727bf231c2a35cdb1ea442003b3a0c1050
tcatonet <catonethomas@yahoo.fr>
2021-10-10 02:23:10 +0200

    logs node2

--- heroku_deploy/api/src/pictures/pictures.service.ts
+++ heroku_deploy/api/src/pictures/pictures.service.ts
@@ -45,7 +45,7 @@ export class PicturesService {
    // console.log(this.objectStorageService)
   //  console.log("objectStorageService ci dessus")
   
-  const picturesList = (await this.pictureModel.find({ challenge }).exec()).map((picture) =>{
+  const picturesList = (await this.pictureModel.find().exec()).map((picture) =>{
     console.log("picture.challenge OK")
     console.log(picture.challenge)
     console.log("challenge OK")
