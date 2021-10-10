0ae6e8c9aaa37dbb6a3cb60880b2c11e30643633
tcatonet <catonethomas@yahoo.fr>
2021-10-10 19:45:21 +0200

    update algo eval code

--- heroku_deploy/api/src/exec-server/godbox.repository.ts
+++ heroku_deploy/api/src/exec-server/godbox.repository.ts
@@ -100,6 +100,7 @@ export class GodBoxRepository {
    "    } \n"+
    "   print(payload) \n"
    
+    console.log(main_algo)
 
     zip.addLocalFolder(`${ALGO_DIR}/${language.name}`, 'evaluation_code');
 
