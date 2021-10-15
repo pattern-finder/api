c8311c19d594df5657ee835f06c2bd1f6db6d3ca
tcatonet <catonethomas@yahoo.fr>
2021-10-15 19:10:33 +0200

    test imtégration plagiat

--- heroku_deploy/api/src/exec-server/godbox.repository.ts
+++ heroku_deploy/api/src/exec-server/godbox.repository.ts
@@ -90,6 +90,7 @@ export class GodBoxRepository {
     "from evaluation_code.evalCommentaire import excecEvalCommentaire \n"+
     "from evaluation_code.evalRedondance import excecEvalRedondance \n"+
     "from evaluation_code.evalVariableName import excecEvalVariableName \n"+
+    "from evaluation_code.excecEvalPlagiat import evalPlagiat \n"+
     "if __name__ == '__main__': \n"+
     "    import os \n"+
     "    import re \n"+
