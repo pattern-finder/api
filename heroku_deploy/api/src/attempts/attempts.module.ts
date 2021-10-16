d7e7a41eccbec8680736e51b4bbbcb55f763b800
tcatonet <catonethomas@yahoo.fr>
2021-10-16 22:21:20 +0200

    try add codeplagiat table

--- heroku_deploy/api/src/attempts/attempts.module.ts
+++ heroku_deploy/api/src/attempts/attempts.module.ts
@@ -4,6 +4,7 @@ import { AttemptsController } from './attempts.controller';
 import { Attempt, AttemptSchema } from './attempt.schema';
 import { AttemptsService } from './attempts.service';
 import { ChallengesModule } from 'src/challenges/challenges.module';
+import { CatsModule } from 'src/cat/cat.module';
 import { ExecServerModule } from 'src/exec-server/exec-server.module';
 import { LanguagesModule } from 'src/languages/languages.module';
 import { ExecBootstrapsModule } from 'src/exec-bootstrap/exec-bootstraps.module';
@@ -15,6 +16,7 @@ import { ExecBootstrapsModule } from 'src/exec-bootstrap/exec-bootstraps.module'
     ExecServerModule,
     LanguagesModule,
     ExecBootstrapsModule,
+    CatsModule,
   ],
   providers: [AttemptsService],
   exports: [AttemptsService],
