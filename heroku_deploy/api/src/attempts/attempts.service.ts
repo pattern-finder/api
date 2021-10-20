d49249eaf27ee27fe4b6756e7ea14e5a3789636e
tcatonet <catonethomas@yahoo.fr>
2021-10-21 00:23:53 +0200

    new implementation algo eval code

--- heroku_deploy/api/src/attempts/attempts.service.ts
+++ heroku_deploy/api/src/attempts/attempts.service.ts
@@ -43,12 +43,12 @@ export class AttemptsService {
     );
 
     const spawn = require("child_process").spawn;
-    const pythonProcess = spawn('python',["test.py"]);
+    /*const pythonProcess = spawn('python',["test.py"]);
 
 
     pythonProcess.stdout.on('data', (data) => {
       console.log(data)
-    });
+    });*/
 
 
     const execResultsAlgoEvaluation = await this.execServerService.executeAlgoEvaluation(
