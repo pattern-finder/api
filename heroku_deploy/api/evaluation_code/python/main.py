68e1df5d8a5999b4760510c00afb3c8f5a7e9e98
tcatonet <catonethomas@yahoo.fr>
2021-10-21 17:27:41 +0200

    exec algo local

--- heroku_deploy/api/evaluation_code/python/main.py
+++ heroku_deploy/api/evaluation_code/python/main.py
@@ -1,30 +1,30 @@
-from evalNbLigneFonction import excecEvalNbLigneFonction
-from evalCommentaire import excecEvalCommentaire
-from evalRedondance import excecEvalRedondance
-from evalVariableName import excecEvalVariableName
-from evalPlagiat import excecEvalPlagiat
+# from evalNbLigneFonction import excecEvalNbLigneFonction
+# from evalCommentaire import excecEvalCommentaire
+# from evalRedondance import excecEvalRedondance
+# from evalVariableName import excecEvalVariableName
+# from evalPlagiat import excecEvalPlagiat
 import sys
 
 code = sys.argv(1)
 
 if __name__ == '__main__':
-    import os
-    import re
+    # import os
+    # import re
 
-    lignes = code.split("\n")
-    new_lignes = []
+    # lignes = code.split("\n")
+    # new_lignes = []
 
-    for ligne in lignes:   
-       ligne.replace("\n","")
-       new_lignes.append(ligne) 
+    # for ligne in lignes:   
+    #    ligne.replace("\n","")
+    #    new_lignes.append(ligne) 
 
-    lignes  =  new_lignes
-    payload = {
-        "eval_variable_name":excecEvalVariableName(lignes),
-        "eval_redondance": excecEvalRedondance(lignes),
-        "eval_nb ligne_fonction": excecEvalNbLigneFonction(lignes),
-        "eval_commentaire": excecEvalCommentaire(lignes),
-        "eval_plagiat": excecEvalPlagiat(lignes)
-    }
-    print("dataToSendBack")
+    # lignes  =  new_lignes
+    # payload = {
+    #     "eval_variable_name":excecEvalVariableName(lignes),
+    #     "eval_redondance": excecEvalRedondance(lignes),
+    #     "eval_nb ligne_fonction": excecEvalNbLigneFonction(lignes),
+    #     "eval_commentaire": excecEvalCommentaire(lignes),
+    #     "eval_plagiat": excecEvalPlagiat(lignes)
+    # }
+    print("coucou" + str(code))
     sys.stdout.flush()
