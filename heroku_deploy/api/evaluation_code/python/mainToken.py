847ff11d31d763656aa6d02527703a80209c1252
tcatonet <catonethomas@yahoo.fr>
2021-10-21 19:24:32 +0200

    update get tokenCode

--- heroku_deploy/api/evaluation_code/python/mainToken.py
+++ heroku_deploy/api/evaluation_code/python/mainToken.py
@@ -5,18 +5,8 @@ import sys
 code = sys.argv[1]
 
 if __name__ == '__main__':
-    import os
-    import re
 
-    lignes = code.split("\n")
-    new_lignes = []
-
-    for ligne in lignes:   
-       ligne.replace("\n","")
-       new_lignes.append(ligne) 
-
-    lignes  =  new_lignes
-    res = excecGetTokenizeCode(lignes, False)
+    res = excecGetTokenizeCode(code, False)
 
     
     print(res)
