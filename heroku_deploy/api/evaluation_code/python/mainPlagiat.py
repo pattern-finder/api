5812f80a7e8a914be48f50ab419c8286fcc75708
tcatonet <catonethomas@yahoo.fr>
2021-10-22 14:52:52 +0200

    update tokenization

--- heroku_deploy/api/evaluation_code/python/mainPlagiat.py
+++ heroku_deploy/api/evaluation_code/python/mainPlagiat.py
@@ -25,5 +25,5 @@ if __name__ == '__main__':
 
         res = excecEvalPlagiat(c, tab_pattern)
 
-    print(str(tab_pattern) +" "+ str(code) +" "+str(res))
+    print(str(user_pattern) +" "+ str(code) +" "+str(res))
     sys.stdout.flush()
