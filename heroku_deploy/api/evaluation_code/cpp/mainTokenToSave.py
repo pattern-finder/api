d22165ed51e45266af4fb1f6beb1f2dd1f4f7cfc
tcatonet <catonethomas@yahoo.fr>
2021-10-22 15:36:09 +0200

    update token generation

--- heroku_deploy/api/evaluation_code/cpp/mainTokenToSave.py
+++ heroku_deploy/api/evaluation_code/cpp/mainTokenToSave.py
@@ -10,6 +10,7 @@ if __name__ == '__main__':
     res = excecGetTokenizeCode(code, True)
             
     res=res.replace("\\","")
+    res = res.replace("\n", "")
     res=res.replace("\"","")
     print(res)
     sys.stdout.flush()
