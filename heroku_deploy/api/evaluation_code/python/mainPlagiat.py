a690005fb96bae58792639e8cefa2cd39119667b
tcatonet <catonethomas@yahoo.fr>
2021-10-22 02:08:48 +0200

    update attemp

--- heroku_deploy/api/evaluation_code/python/mainPlagiat.py
+++ heroku_deploy/api/evaluation_code/python/mainPlagiat.py
@@ -11,7 +11,8 @@ if __name__ == '__main__':
 
     res = False
     for c in code:
+        c = c.replace("\n", "")
         res = excecEvalPlagiat(c, user_pattern)
 
-    print(res)
+    print(user_pattern +"\n"+ code +"\n"+res)
     sys.stdout.flush()
